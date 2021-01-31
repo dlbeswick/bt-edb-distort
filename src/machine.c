d7bc8a518a64d86bb7455a4e0f856109ef3f860d
David Beswick <dlbeswick@gmail.com>
2021-01-31 22:43:06 +1100

    Most correct caps handling, much simplified.

--- src/machine.c
+++ src/machine.c
@@ -184,89 +184,55 @@ static gboolean set_caps(
   GstCaps* incaps,
   GstCaps* outcaps) {
 
-  BtEdbDistortInternal* const self = (BtEdbDistortInternal*)trans;
-  
   GST_INFO("incaps %s", gst_caps_to_string(incaps));
   GST_INFO("outcaps %s", gst_caps_to_string(outcaps));
 
-  GstPad* convert_in_src = gst_element_get_static_pad(self->convert_in, "src");
-  GstCaps* caps_src = gst_pad_get_current_caps(convert_in_src);
-  gst_object_unref(convert_in_src);
-
-  GST_INFO("caps_src %p", caps_src);
-
-  // Renegotiate caps if distort's output rate doesn't match the target resampling rate, compared to the rate of
-  // the upstream source. See query_distort_sink; there should be a better way.
-  if (caps_src) {
-    gint rate_convert_in_src;
-    gboolean ret = gst_structure_get_int(gst_caps_get_structure(caps_src, 0), "rate", &rate_convert_in_src);
-    g_assert(ret);
-
-    gst_caps_unref(caps_src);
-    caps_src = 0;
-    
-    gint rate;
-    ret = gst_structure_get_int(gst_caps_get_structure(incaps, 0), "rate", &rate);
-    g_assert(ret);
-    
-    if (rate != rate_convert_in_src * self->oversample) {
-      GstPad* pad = gst_element_get_static_pad((GstElement*)trans, "sink");
-      gst_pad_push_event(pad, gst_event_new_reconfigure());
-      gst_object_unref(pad);
-    }
-  }
-  
   return TRUE;
 }
 
 /*
   This callback is attached to the "sink" pad of the internal Distort effect.
   It's called at the start of playback, when GStreamer is figuring out how to fixate caps for all the machines.
+  The goal is to force the upstream audioconvert element to use an oversampled rate on its "src" pad.
 
-  What the code is aiming to do is to figure out what rate has been decided for the src pad of the upstream
-  (audioresample), and then fixing the rate of its own sink pad to the target oversampling rate.
-  
-  It will be called both when caps have been fixed on the downstream audioresample's pad and when it hasn't. Nothing
-  is done if caps haven't been fixed there, yet.
-
-  What seems to happen is that one round of negotiation is done where upstream caps aren't available, then distort's
-  "set_caps" function calls for a renegotiate if a rate change is still required, then this will be called again with
-  current caps available. But there must be a way to avoid the renegotiate? I had trouble hooking "event" and "query"
-  handlers to audioresample.
+  This is called many times during negotiation, with caps becoming more specific as the negotiation progresses.
+  Initially, there are only caps with a single structure having ranges.
+  Then, there are caps with fixed and non-fixed structures as more information comes from upstream.
+  At this point, "distort" can fix its own caps which will determine the oversampling rate from audioconvert.
  */
 static gboolean query_distort_sink(GstPad *pad, GstObject* parent, GstQuery* query) {
   BtEdbDistortInternal* self = (BtEdbDistortInternal*)parent;
   
   switch(GST_QUERY_TYPE (query)) {
   case GST_QUERY_CAPS: {
-    // Note: parent is BtEdbDistort
-    GstPad* convert_in_src = gst_element_get_static_pad(self->convert_in, "sink");
-    GstCaps* caps_convert_in_src = gst_pad_get_current_caps(convert_in_src);
-    GstCaps* caps_sink = gst_caps_make_writable(gst_pad_get_pad_template_caps(pad));
+    GstCaps* caps_in;
+    gst_query_parse_caps(query, &caps_in);
+    GST_INFO("query caps_in %s", gst_caps_to_string(caps_in));
 
-    GST_INFO("query sink %s", gst_caps_to_string(caps_sink));
-    GST_INFO("query convert_in_src %s", gst_caps_to_string(caps_convert_in_src));
+    // If there are no caps in the query, then there is no information on which to act.
+    // If the incoming caps are already fixed, then the final oversampled rate has already been presented upstream
+    // and there is nothing to do.
+    if (caps_in && !gst_caps_is_fixed(caps_in)) {
+      // At this point the incoming caps will present one or more structures that may or may not be fixed.
+      // Find the first structure having a fixed "rate" value. This is the sample rate coming from upstream.
+      // If found, then present the fixed caps with oversampled rate to the upstream audioresample element.
+      // If there is no fixed rate value, then negotiation still hasn't progressed far enough to be able to fix
+      // "distort"'s caps.
+      for (gint i = 0; i < gst_caps_get_size(caps_in); i++) {
+        GstStructure* structure = gst_caps_get_structure(caps_in, i);
 
-    if (caps_convert_in_src) {
-      gint out_rate;
-      gst_structure_get_int(gst_caps_get_structure(caps_convert_in_src, 0), "rate", &out_rate);
-      gst_caps_unref(caps_convert_in_src);
-      caps_convert_in_src = 0;
-
-      GST_INFO("convert_out_src rate %d", out_rate);
-    
-      for (gint i = 0; i < gst_caps_get_size(caps_sink); i++) {
-        GstStructure* structure = gst_caps_get_structure(caps_sink, i);
-
-        gst_structure_fixate_field_nearest_int(structure, "rate", out_rate * self->oversample);
+        int rate;
+        if (gst_structure_get_int(structure, "rate", &rate)) {
+          GstCaps* caps_out = gst_caps_copy_nth(caps_in, i);
+          gst_structure_set(gst_caps_get_structure(caps_out, 0), "rate", G_TYPE_INT, rate * self->oversample, NULL);
+          GST_INFO("query caps_out %s", gst_caps_to_string(caps_out));
+          gst_query_set_caps_result(query, caps_out);
+          return TRUE;
+        }
       }
-
-      GST_INFO("result %s", gst_caps_to_string(caps_sink));
     }
     
-    gst_query_set_caps_result(query, caps_sink);
-    gst_caps_unref(caps_sink);
-    return TRUE;
+    return gst_pad_query_default(pad, parent, query);
   }
   default:
     return gst_pad_query_default(pad, parent, query);
